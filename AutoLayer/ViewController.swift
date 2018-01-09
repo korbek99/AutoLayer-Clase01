8a00b40c754a3189f9538f0a5b48d8930f66d62e
korbek100 <korbek100@gmail.com>
2018-01-08 23:51:17 -0300

    Initial commit

--- AutoLayer/ViewController.swift
+++ AutoLayer/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIAlertViewDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let alertView = UIAlertView(title: "Titulo de mensaje", message: "Welcome to Auto Layaout", delegate: self, cancelButtonTitle: "Cancel", otherButtonTitles: "OK")
+        alertView.alertViewStyle = .Default
+        alertView.show()
+        
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+ 
+    func alertView(alertView: UIAlertView, clickedButtonAtIndex buttonIndex: Int) {
+        
+    let buttonTitle = alertView.buttonTitleAtIndex(buttonIndex)
+        println("\(buttonIndex) pressed")
+        if buttonTitle == "Yes"
+        {
+            exit(1)
+            
+        }
+        
+    }
 }
 
